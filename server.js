0672119754fda5ef693c5b3b7915db3f1f70e900
ebenadda <ebenadda@amazon.com>
2022-07-20 21:31:28 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+// app.get("/", function (req, res) {
+//   res.send("Hello World");
+// });
+// app.listen(3000);
+app.listen(3000, function () {
+  console.log("listening on 3000");
+});
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+});
